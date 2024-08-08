b4eea27e07b37cd9cfe491c29cc7efce2b162b81
Bharatmish <bm4957@srmist.edu.in>
2024-08-09 00:36:06 +0530

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: './src',
+  root: 'src', // Make sure this is set to the directory containing index.html
+  build: {
+    outDir: '../dist', // Output directory for the build
+    rollupOptions: {
+      input: 'src/index.html' // Adjust this path if needed
+    }
+  }
 });
